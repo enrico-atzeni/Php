3a712b96dc94b1553a35e09d3de412cee53f2ba6
atzeniEn <atzenien@gmail.com>
2017-12-04 20:15:30 +0100

    aggiunto snippet debug performance

---
+++ src/alittlehelpersnippet.php
@@ -0,0 +1,68 @@
+<?php
+/*
+
+	DEBUG CODE:
+		paste it in separate sections of your template
+
+*/
+?>
+
+
+<?php /* A LITTLE HELPER SNIPPET GENERAL */
+global $memoryFUNC;
+if(!$memoryFUNC)
+	$memoryFUNC = array();
+$SUPER_DEBUG_CONT = isset($SUPER_DEBUG_CONT) ? $SUPER_DEBUG_CONT+1 : 0;
+$thisVal = array('file' => __FILE__.':'.$SUPER_DEBUG_CONT, 'val' => (memory_get_peak_usage(true) / 1024),'vars'=>count(array_keys(get_defined_vars())));
+$microTimePost = round(microtime(true) *1000);
+if(isset($microTimePre)){
+	$thisVal['time'] = ($microTimePost-$microTimePre);
+	$microTimePre = $microTimePost;
+}else $microTimePre = $microTimePost;
+array_push($memoryFUNC, $thisVal);
+/* / A LITTLE HELPER SNIPPET GENERAL */ ?>
+
+
+
+
+
+<?php /* A LITTLE HELPER SNIPPET FOR WordPress */
+global $memoryFUNC;
+if(!$memoryFUNC)
+	$memoryFUNC = array();
+$SUPER_DEBUG_CONT = isset($SUPER_DEBUG_CONT) ? $SUPER_DEBUG_CONT+1 : 0;
+$thisVal = array('file' => str_replace(ABSPATH,'',__FILE__).':'.$SUPER_DEBUG_CONT, 'val' => (memory_get_peak_usage(true) / 1024),'vars'=>count(array_keys(get_defined_vars())));
+$microTimePost = round(microtime(true) *1000);
+if(isset($microTimePre)){
+	$thisVal['time'] = ($microTimePost-$microTimePre);
+	$microTimePre = $microTimePost;
+}else $microTimePre = $microTimePost;
+array_push($memoryFUNC, $thisVal);
+/* / A LITTLE HELPER SNIPPET FOR WordPress */ ?>
+
+
+
+
+
+<?php
+/*
+
+	OUTPUT RESULTS:
+		paste it in the very end of your template (ex: footer.php)
+		I used $_GET['debug'] to print it only when i pass the "debug" querystring to the url, like adding ?debug
+
+*/
+?>
+
+
+<?php if(isset($_GET['debug'])){
+	global $memoryFUNC;
+	$precVal = 0;
+	foreach($memoryFUNC as $mem){
+		$vars = isset($mem['vars']) ? $mem['vars'] : 'n.d.';
+		echo $mem['file'].' - '.$mem['val'].' KB - differenza dal precedente: <b>'.($mem['val'] - $precVal).'</b>, vars: '.$vars.', time elapsed: '.$mem['time'].'<br />';
+		$precVal = $mem['val'];
+	};
+	$memoryFUNC = null;
+} ?>
+
